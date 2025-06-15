ea747fb6e827fd823ed37ae86bb498f41274963a
josh9969 <bigjosher99@gmail.com>
2025-06-15 10:40:39 +0400

    Update fitness_app.py

--- fitness_app.py
+++ fitness_app.py
@@ -51,7 +51,7 @@ st.markdown("""
         font-family: 'Trebuchet MS', sans-serif;
     }
     .stButton > button {
-        background-color: #7F8CAA;
+        background-color: #123458;
         color: black;
         border-radius: 8px;
         padding: 0.5em 1em;
